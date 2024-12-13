2be750a82391dcccbb1f9e2a69abd6db12167992
Sherloque <sherloque7@gmail.com>
2024-12-13 17:22:42 +0100

    React version bump and further improvement and refactor of existing core files

--- front/src/App.js
+++ front/src/App.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import { Provider} from 'react-redux';
-import { Router, Route} from 'react-router-dom';
-import MainPage from 'MainPage/MainPage';
-import SearchPage from 'SearchPage/SearchPage';
-import LoginPage from 'LoginPage/LoginPage';
-import SignUpPage from 'SignUpPage/SignUpPage';
-import ProfilePage from 'ProfilePage/ProfilePage';
-import store from './store/store'
-import history from './history/history';
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "MainPage/MainPage";
+import SearchPage from "SearchPage/SearchPage";
+import LoginPage from "LoginPage/LoginPage";
+import SignUpPage from "SignUpPage/SignUpPage";
+import ProfilePage from "ProfilePage/ProfilePage";
+import store from "./store/store";
 
 function App() {
-	
-	return (
-		<Provider store={store}>
-		<Router history={history}>
-			<div>
-				<Route path="/" component={MainPage} exact />
-				<Route path="/login" component={LoginPage} exact />
-				<Route path="/signup" component={SignUpPage} exact />
-				<Route path="/profile" component={ProfilePage} exact />
-				<Route path="/search" component={SearchPage} exact />
-			</div>
-		</Router>
-		</Provider>
-	)
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
 }
-
 
 export default App;
