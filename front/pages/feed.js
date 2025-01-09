fc119aa427a33e27891c92209b9fb86bcfbf793f
Sherloque <sherloque7@gmail.com>
2025-01-09 19:19:47 +0100

    fixed styles for main page

--- front/pages/feed.js
+++ front/pages/feed.js
@@ -17,7 +17,6 @@ import {
 } from "../src/store/reducers/explore.reducer";
 import { logoutUser } from "../src/store/reducers/auth.reducer";
 import debounce from "lodash.debounce";
-import bannerImg from "../public/banner.png";
 
 import Player from "../components/Player";
 import SongList from "../components/SongList";
@@ -63,64 +62,76 @@ const MainPage = () => {
   );
 
   return (
-    <div className={styles.mainpageContainer}>
-      <header className={styles.mainpageHeader}>
-        <div className={styles.mainpageHeaderPlatformname}>PLATFORMNAME</div>
-        <nav className={styles.mainpageNav}>
+    <div className={`${styles["mainpage-container"]}`}>
+      <header className={`${styles["mainpage-header"]}`}>
+        <div className={`${styles["mainpage-header-platformname"]}`}>
+          PLATFORMNAME
+        </div>
+        <nav className={`${styles["mainpage-nav"]}`}>
           {token ? (
             <>
-              <Link href="/profile" className={styles.navLink}>
-                Profile <ProfileIcon className={styles.navIcon} />
+              <Link
+                href="/profile"
+                className={`${styles["mainpage-nav-link"]}`}
+              >
+                Profile <ProfileIcon className={`${styles["mainpage-icon"]}`} />
               </Link>
-              <Link href="#" className={styles.navLink} onClick={handleLogout}>
-                Logout <LogoutIcon className={styles.navIcon} />
+              <Link
+                href="#"
+                className={`${styles["mainpage-nav-link"]}`}
+                onClick={handleLogout}
+              >
+                Logout <LogoutIcon className={`${styles["mainpage-icon"]}`} />
               </Link>
             </>
           ) : (
             <>
-              <Link href="/signup" className={styles.navLink}>
-                Registration <SignUpIcon className={styles.navIcon} />
+              <Link href="/signup" className={`${styles["mainpage-nav-link"]}`}>
+                Registration{" "}
+                <SignUpIcon className={`${styles["mainpage-icon"]}`} />
               </Link>
-              <Link href="/login" className={styles.navLink}>
-                Login <LoginIcon className={styles.navIcon} />
+              <Link href="/login" className={`${styles["mainpage-nav-link"]}`}>
+                Login <LoginIcon className={`${styles["mainpage-icon"]}`} />
               </Link>
             </>
           )}
         </nav>
       </header>
 
-      <main className={styles.mainpageMain}>
+      <main className={`${styles["mainpage-main"]}`}>
         <>
-          <section className={styles.welcomeSection}>
+          <section className={`${styles["welcome-section"]}`}>
             {token && (
-              <div className={styles.welcomeSectionGreeting}>
+              <div className={`${styles["welcome-section-greeting"]}`}>
                 <h1>Hey, {userLogin}!</h1>
               </div>
             )}
-            <div className={styles.banner}>
-              <div className={styles.bannerInfo}>
-                <h2 className={styles.bannerInfoPlatformname}>PLATFORMNAME</h2>
-                <div className={styles.bannerInfoBlurWrapper}>
-                  <h2 className={styles.bannerInfoSubtitle}>
+            <div className={`${styles["banner"]}`}>
+              <div className={`${styles["banner-info"]}`}>
+                <h2 className={`${styles["banner-info-platformname"]}`}>
+                  PLATFORMNAME
+                </h2>
+                <div className={`${styles["banner-info-blur-wrapper"]}`}>
+                  <h2 className={`${styles["banner-info-subtitle"]}`}>
                     Discover, Share, Upload!
                   </h2>
                 </div>
-                <p className={styles.bannerInfoDescription}>
+                <p className={`${styles["banner-info-description"]}`}>
                   Upload Your Beat, Feel the Music Heat.
                 </p>
               </div>
               <img
-                src={bannerImg}
+                src="/assets/banner.png"
                 alt="Music Banner"
-                className={styles.bannerImage}
+                className={`${styles["banner-image"]}`}
               />
             </div>
           </section>
 
-          <section className={styles.chartSection}>
+          <section className={`${styles["chart-section"]}`}>
             <input
               type="text"
-              className={styles.chartSectionSearch}
+              className={`${styles["chart-section-search"]}`}
               placeholder="Search..."
               onChange={debouncedSearch}
             />
@@ -150,7 +161,7 @@ const MainPage = () => {
             )}
           </section>
 
-          <section className={styles.uploadsSection}>
+          <section className={`${styles["uploads-section"]}`}>
             <h2>Recent Uploads</h2>
             <SongList
               fetchAction={fetchRecentUploads}
@@ -162,13 +173,21 @@ const MainPage = () => {
         </>
         {track && <Player track={track}></Player>}
       </main>
-      <footer className={styles.mainpageFooter}>
+      <footer className={`${styles["mainage-footer"]}`}>
         <p>&copy; 2024 PLATFORMNAME. All rights reserved.</p>
       </footer>
-      <div className={styles.backgroundEffect1}></div>
-      <div className={styles.backgroundEffect2}></div>
-      <div className={styles.backgroundEffect3}></div>
-      <div className={styles.backgroundEffect4}></div>
+      <div
+        className={`${styles["background-effect"]} ${styles["effect-1"]}`}
+      ></div>
+      <div
+        className={`${styles["background-effect"]} ${styles["effect-2"]}`}
+      ></div>
+      <div
+        className={`${styles["background-effect"]} ${styles["effect-3"]}`}
+      ></div>
+      <div
+        className={`${styles["background-effect"]} ${styles["effect-4"]}`}
+      ></div>
     </div>
   );
 };
